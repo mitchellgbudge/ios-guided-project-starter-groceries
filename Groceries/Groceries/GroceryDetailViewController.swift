135f0918d04d5700f6dcbaf4c6f5a9d66afc11c2
Ben Gohlke <jcgohlke@me.com>
2019-08-05 10:44:26 -0400

    Updated starter project to include more code; designed for the 2 hour lecture constraint

---
+++ Groceries/Groceries/GroceryDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  GroceryDetailViewController.swift
+//  Groceries
+//
+//  Created by Ben Gohlke on 7/8/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class GroceryDetailViewController: UIViewController {
+    
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var aisleLabel: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
