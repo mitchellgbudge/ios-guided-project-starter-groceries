f269d428751e96ce89dabda680091fd56440304a
Mitchell Budge <mitchellbudge@gmail.com>
2019-08-05 11:21:40 -0600

    Reconfigure TV methods and data source

--- Groceries/Groceries/GroceriesTableViewController.swift
+++ Groceries/Groceries/GroceriesTableViewController.swift
@@ -10,19 +10,28 @@ import UIKit
 
 class GroceriesTableViewController: UITableViewController {
 
+    // MARK: - Properties
+    
+    var groceries: [(name: String, aisle: Int, category: String, count: Int)] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadGroceries()
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return groceries.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "GroceryItemCell", for: indexPath) as? GroceryItemTableViewCell else { return UITableViewCell() }
-        
+        let grocery = groceries[indexPath.row]
+        cell.nameLabel.text = grocery.name
+        cell.categoryLabel.text = grocery.category
+        cell.countLabel.text = "\(grocery.count)"
         return cell
     }
 
@@ -37,13 +46,14 @@ class GroceriesTableViewController: UITableViewController {
     
     private func loadGroceries()
     {
-//        let carrots = (name: "carrots", aisle: 4, category: "produce", count: 25)
-//        let soup = (name: "tomato soup", aisle: 6, category: "canned goods", count: 100)
-//        let cereal = (name: "Frosted Flakes", aisle: 2, category: "cereals", count: 43)
-//        let pringles = (name: "Pringles", aisle: 7, category: "chips", count: 15)
-//        let tostitos = (name: "Tostitos", aisle: 7, category: "chips", count: 26)
-//        let seafood = (name: "crab legs", aisle: 1, category: "seafood", count: 8)
-//        let rice = (name: "Minute Rice", aisle: 2, category: "dry rice and beans", count: 62)
-//        let turkey = (name: "Boar's Head turkey", aisle: 9, category: "deli", count: 4)
+        let carrots = (name: "Carrots", aisle: 4, category: "produce", count: 25)
+        let soup = (name: "Tomato soup", aisle: 6, category: "canned goods", count: 100)
+        let cereal = (name: "Frosted Flakes", aisle: 2, category: "cereals", count: 43)
+        let pringles = (name: "Pringles", aisle: 7, category: "chips", count: 15)
+        let tostitos = (name: "Tostitos", aisle: 7, category: "chips", count: 26)
+        let seafood = (name: "Crab legs", aisle: 1, category: "seafood", count: 8)
+        let rice = (name: "Minute Rice", aisle: 2, category: "dry rice and beans", count: 62)
+        let turkey = (name: "Boar's Head turkey", aisle: 9, category: "deli", count: 4)
+        groceries.append(contentsOf: [carrots, soup, cereal, pringles, tostitos, seafood, rice, turkey])
     }
 }
